8d2bc6a10e3dc525d9adc0da4e28cdf46b7ba7a2
rosymaple <156586504+rosymaple@users.noreply.github.com>
2025-03-19 18:28:31 +0000

    tried to add quick pb&g to recipes

--- app.py
+++ app.py
@@ -7,10 +7,17 @@ app = Flask(__name__)
 # Sample recipe data stored in a dictionary
 # Each recipe has a unique key (recipe_id) and contains name, ingredients list, and instructions
 recipes = {
+    
     "midnight-ramen": {
         "name": "Midnight Ramen",
         "ingredients": ["instant ramen", "egg", "green onions", "hot sauce"],
         "instructions": "1. Boil water\n2. Cook noodles\n3. Add toppings"
+    }
+
+    "quick-pbj": {
+    "name": "Quick PB&J",
+    "ingredients": ["bread", "peanut butter", "jelly"],
+    "instructions": "1. Spread peanut butter & jelly on slices\n2. Put them together"
     }
 }
 
